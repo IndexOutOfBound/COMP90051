954673745f11dc49d186e1c8842eb119c2e9e519
IndexOutOfBound <Weikai.zeng95@gmail.com>
2019-09-01 05:46:49 +1000

    add a keras model

---
+++ Keras.py
@@ -0,0 +1,73 @@
+import itertools
+import os
+
+# % matplotlib
+from docutils.nodes import inline
+
+from Pre_Process import df
+
+inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.preprocessing import text, sequence
+from keras import utils
+
+train_size = int(len(df) * .7)
+train_posts = df['Post'][:train_size]
+train_tags = df['ID'][:train_size]
+All_tags = df['ID']
+
+test_posts = df['Post'][train_size:]
+test_tags = df['ID'][train_size:]
+
+
+max_words = 1000
+tokenize = text.Tokenizer(num_words=max_words, char_level=False)
+tokenize.fit_on_texts(train_posts)  # only fit on train
+
+x_train = tokenize.texts_to_matrix(train_posts)
+x_test = tokenize.texts_to_matrix(test_posts)
+
+encoder = LabelEncoder()
+encoder.fit(All_tags)
+y_train = encoder.transform(train_tags)
+y_test = encoder.transform(test_tags)
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
+
+batch_size = 32
+epochs = 2
+
+# Build the model
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
+# evaluate the model
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test accuracy:', score[1])
